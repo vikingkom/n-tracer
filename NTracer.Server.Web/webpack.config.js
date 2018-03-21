246eaf552791c0b87b54f9bfd0f70e4c6bbd94be
vikingkom <vkomarevskiy@gmail.com>
2018-03-22 01:29:00 +0300

    index.html generation done

--- NTracer.Server.Web/webpack.config.js
+++ NTracer.Server.Web/webpack.config.js
@@ -2,13 +2,17 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const bundleOutputDir = './wwwroot/dist';
 
 module.exports = (env) => {
     const isDevBuild = !(env && env.prod);
     return [{
         stats: { modules: false },
-        entry: { 'main': './ClientApp/boot.tsx' },
+        entry: { 
+            'main': './ClientApp/boot.tsx',
+            'vendor': ['isomorphic-fetch', 'react', 'react-dom', 'react-router-dom', 'jquery'] 
+        },
         resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
         output: {
             path: path.join(__dirname, bundleOutputDir),
@@ -19,15 +23,17 @@ module.exports = (env) => {
             rules: [
                 { test: /\.tsx?$/, include: /ClientApp/, use: 'awesome-typescript-loader' },
                 { test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : ExtractTextPlugin.extract({ use: 'css-loader?minimize' }) },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+                { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: 'url-loader?limit=25000' }
             ]
         },
         plugins: [
+
             new CheckerPlugin(),
             new webpack.DllReferencePlugin({
                 context: __dirname,
                 manifest: require('./wwwroot/dist/vendor-manifest.json')
-            })
+            }),
+            new HtmlWebpackPlugin({template: './ClientApp/index.template.html',filename: 'index.html'}),
         ].concat(isDevBuild ? [
             // Plugins that apply in development builds only
             new webpack.SourceMapDevToolPlugin({
